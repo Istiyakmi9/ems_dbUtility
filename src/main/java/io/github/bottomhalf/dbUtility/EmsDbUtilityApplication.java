cf5025c99631a80d95fafdde7400a6291ed8bb98
istiyakmi9 <istiyaq.mi9@gmail.com>
2023-11-29 13:17:50 +0530

    first commit

---
+++ src/main/java/io/github/bottomhalf/dbUtility/EmsDbUtilityApplication.java
@@ -0,0 +1,13 @@
+package io.github.bottomhalf.dbUtility;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsDbUtilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmsDbUtilityApplication.class, args);
+	}
+
+}
